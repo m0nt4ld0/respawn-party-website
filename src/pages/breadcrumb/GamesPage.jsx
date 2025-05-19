900b758b22fdde7002358ca88cb9e7b32631dd39
Mariela Montaldo <mmontaldo@live.com>
2025-05-18 23:34:46 -0300

    Add: Breadcrumb

--- src/pages/breadcrumb/GamesPage.jsx
+++ src/pages/breadcrumb/GamesPage.jsx
@@ -1,7 +1,8 @@
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { Card, Container, Row, Col, Spinner, Breadcrumb } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import Content from '../../layouts/Content';
 import Product from '../shoppingCart/Product';
-
 
 function GamesPage() {
   const { state } = useLocation();
@@ -12,17 +13,26 @@ function GamesPage() {
   }
 
   return (
-    <Content>   
-      <h1>Juegos para {consoleData.Name}</h1>
-      <img src={consoleData.IconURL} 
-           alt={consoleData.Name} 
-           style={{ height: '200px', objectFit: 'contain' }} />
-      <p>Contamos con un equipo de profesionales que te asesoran para que puedas disfrutar de la mejor experiencia de videojuegos.</p>
+    <Content>
+      <h3>Juegos para {consoleData.Name}</h3>
+      <Breadcrumb>
+        <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/' }}>Inicio</Breadcrumb.Item>
+        <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/consoles' }}>Consolas</Breadcrumb.Item>
+        <Breadcrumb.Item active>Juegos para {consoleData.Name}</Breadcrumb.Item>
+      </Breadcrumb>
+      <img
+        src={consoleData.IconURL}
+        alt={consoleData.Name}
+        style={{ height: '200px', objectFit: 'contain' }}
+      />
+      <p>
+        Contamos con un equipo de profesionales que te asesoran para que puedas disfrutar de la
+        mejor experiencia de videojuegos.
+      </p>
 
       <Product consoleId={consoleData.ID} />
     </Content>
   );
 }
-
 
 export default GamesPage;
