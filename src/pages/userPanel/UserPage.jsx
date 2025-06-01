d44c6928a389394baa3ceb4b7648f27879ba653c
Mariela Montaldo <mmontaldo@live.com>
2025-05-31 21:05:45 -0300

    Add: Mejoras esteticas en pagina de usuario, banner superior y header hero

--- src/pages/userPanel/UserPage.jsx
+++ src/pages/userPanel/UserPage.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Content from '../../layouts/Content';
-import { Button, Table, Image } from 'react-bootstrap';
+import { Button, 
+         Table, 
+         Container,
+         Breadcrumb,
+         Image } from 'react-bootstrap';
 import { useAuth } from '../../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 function UserPage() {
   const { user: currentUser, loadingUser, logout } = useAuth();
@@ -41,7 +45,18 @@ function UserPage() {
 
   return (
     <Content>
-      <div className="container mt-6" style={{ maxWidth: '600px', marginTop: '200px' }}>
+      <div className="angled-background" />
+      <Container className="py-4">
+        <div className="header-hero">
+          <h1>Hola, {currentUser?.displayName || currentUser?.email}</h1>
+        </div>
+
+        <Breadcrumb className="bg-transparent px-0 mb-4">
+            <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/' }}>Inicio</Breadcrumb.Item>
+            <Breadcrumb.Item active>Mi perfil</Breadcrumb.Item>
+          </Breadcrumb>
+
+      <div className="container" style={{ maxWidth: '600px'}}>
         <div className="bg-dark text-light p-4 rounded shadow text-center">
           {currentUser?.photoURL && (
             <div className="mb-3">
@@ -82,6 +97,7 @@ function UserPage() {
           </Button>
         </div>
       </div>
+      </Container>
     </Content>
   );
 }
