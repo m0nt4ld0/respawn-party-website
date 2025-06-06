816ea3867247411fc2b2182a257bc3ec680733a7
Mariela Montaldo <mmontaldo@live.com>
2025-06-06 00:39:23 -0300

    Add: Lazy loading on components for optimizing performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, lazy, Suspense } from 'react';
 import { AuthProvider } from "./contexts/AuthContext";
 import ShoppingCartProvider from './contexts/ShoppingCartContext';
 import { ConsoleProvider } from './contexts/ConsoleContext';
 
-import SearchResultsPage from './pages/SearchResultsPage';
-import Homepage from './pages/Homepage';
-import LoginPage from "./pages/LoginPage";
-import ContactPage from './pages/ContactPage';
-import NotFoundPage from './pages/NotFoundPage';
-import AboutPage from './pages/AboutPage';
-import FAQPage from './pages/FAQPage';
-import ProductDetailPage from './pages/breadcrumb/ProductDetailPage';
-import ShoppingCartPage from './pages/ShoppingCartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import ConsolePage from './pages/breadcrumb/ConsolePage';
-import GamesPage from './pages/breadcrumb/GamesPage';
-import UserPage from './pages/userPanel/UserPage';
-import CustomProductPage from './pages/CustomProductPage';
-
 import ProtectedRoute from "./components/ProtectedRoute";
+import ShoppingCartPage from './pages/ShoppingCartPage';
 
 import { auth } from "./api/firebase";
 import { onAuthStateChanged } from "firebase/auth";
@@ -30,41 +16,58 @@ import { ToastContainer } from 'react-toastify';
 import { HelmetProvider } from 'react-helmet-async';
 import 'react-toastify/dist/ReactToastify.css';
 
+const Homepage = lazy(() => import('./pages/Homepage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const ContactPage = lazy(() => import('./pages/ContactPage'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const FAQPage = lazy(() => import('./pages/FAQPage'));
+const SearchResultsPage = lazy(() => import('./pages/SearchResultsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const ProductDetailPage = lazy(() => import('./pages/breadcrumb/ProductDetailPage'));
+
+const CheckoutPage = lazy(() => import('./pages/CheckoutPage'));
+const ConsolePage = lazy(() => import('./pages/breadcrumb/ConsolePage'));
+const GamesPage = lazy(() => import('./pages/breadcrumb/GamesPage'));
+const UserPage = lazy(() => import('./pages/userPanel/UserPage'));
+const CustomProductPage = lazy(() => import('./pages/CustomProductPage'));
+
 function AnimatedRoutes() {
   const location = useLocation();
 
   return (
     <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/user"
-          element={
-            <ProtectedRoute>
-              <UserPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute adminOnly={true}>
-              <CustomProductPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-us" element={<AboutPage />} />
-        <Route path="/contact-us" element={<ContactPage />} />
-        <Route path="/console/:id" element={<GamesPage />} />
-        <Route path="/faq" element={<FAQPage />} />
-        <Route path="/consoles" element={<ConsolePage />} />
-        <Route path="/shopping-cart" element={<ShoppingCartPage />} />
-        <Route path="/product/:id" element={<ProductDetailPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/search" element={<SearchResultsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<div className="text-center p-5">Cargando...</div>}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/user"
+            element={
+              <ProtectedRoute>
+                <UserPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <CustomProductPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about-us" element={<AboutPage />} />
+          <Route path="/contact-us" element={<ContactPage />} />
+          <Route path="/console/:id" element={<GamesPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/consoles" element={<ConsolePage />} />
+          <Route path="/shopping-cart" element={<ShoppingCartPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/search" element={<SearchResultsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </AnimatePresence>
   );
 }
